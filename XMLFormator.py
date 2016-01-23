9476a8106238eb6b63ca3049cc7b78ba92b407fc
周鲁东 <ludongzhou@gmail.com>
2016-01-23 21:08:38 +0800

    写了第一版program xsl,但是合并多个xsl的时候出现错误

--- XMLFormator.py
+++ XMLFormator.py
@@ -3,21 +3,21 @@ from lxml import etree
 
 def transform(xmlPath, xslPath):
     # read xsl file
-    xslRoot = etree.fromstring(open(xslPath).read())
+    xslRoot = etree.fromstring(open(xslPath, encoding='utf-8').read())
 
     transform = etree.XSLT(xslRoot)
 
     # read xml
-    xmlRoot = etree.fromstring(open(xmlPath).read())
+    xmlRoot = etree.fromstring(open(xmlPath, encoding='utf-8').read())
 
     # transform xml with xslt
     transRoot = transform(xmlRoot)
 
     # return transformation result
-    return etree.tostring(transRoot)
+    return etree.tostring(transRoot, encoding='utf-8')
 
 
 if __name__ == '__main__':
-    ans = transform('sample.xml', 'sample.xsl')
-    with open('out.xml', 'w') as outFile:
+    ans = transform('2.xml', '2.xsl')
+    with open('out.xml', 'w', encoding='utf-8') as outFile:
         outFile.write(ans.decode("utf-8"))
