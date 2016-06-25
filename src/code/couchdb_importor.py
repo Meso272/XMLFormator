55c14bfda2702792f0be50a43ea2af2316889b79
周鲁东 <ludongzhou@gmail.com>
2016-06-25 23:22:45 +0800

    fix couchdb_importtor

--- src/code/couchdb_importor.py
+++ src/code/couchdb_importor.py
@@ -1,5 +1,4 @@
-import couchdb, glob, json
-import os
+import couchdb, json, os, logging
 
 
 """
@@ -10,38 +9,52 @@ class Importor:
     def __init__(self, server='192.168.1.106', port='5984', folderPath=''):
         serverUrl = 'http://' + server + ':' + port + '/'
         self.couch = couchdb.Server(url=serverUrl)
-        self.dbvideo = self.couch['video']
-        self.dbprogram = self.couch['program']
-        self.dbsequence = self.couch['sequence']
-        self.dbscene = self.couch['scene']
-        self.dbshot = self.couch['shot']
+        try:
+            self.dbvideo = self.couch['video']
+            self.dbprogram = self.couch['program']
+            self.dbsequence = self.couch['sequence']
+            self.dbscene = self.couch['scene']
+            self.dbshot = self.couch['shot']
+            self.connected = True
+        except TimeoutError:
+            logging.error("can't connect to couchdb: %s" % serverUrl)
+            self.connected = False
         self.folderPath = folderPath
 
-    def importFile(self, filePath):  # 将单个文件导入couchdb, filePath为文件路径
-        fileClass = filePath.split('/')[-1]
-        with open(filePath) as jsonFile:
-            string = json.load(jsonFile)
-            if fileClass.startswith('Video'):
-                _id = string["Metadata"]["VideoID"]
-                self.dbvideo.save(string, )
-            elif fileClass.startswith('Program'):
-                self.dbprogram.save(string)
-            elif fileClass.startswith('Sequence'):
-                self.dbsequence.save(string)
-            elif fileClass.startswith('Scene'):
-                self.dbscene.save(string)
-            elif fileClass.startswith('Shot'):
-                self.dbshot.save(string)
+    def importFile(self, json_string, fileClass):  # 将单个文件导入couchdb, filePath为文件路径
+        if fileClass.startswith('Video'):
+            self.dbvideo.save(json_string)
+        elif fileClass.startswith('Program'):
+            self.dbprogram.save(json_string)
+        elif fileClass.startswith('Sequence'):
+            self.dbsequence.save(json_string)
+        elif fileClass.startswith('Scene'):
+            self.dbscene.save(json_string)
+        elif fileClass.startswith('Shot'):
+            self.dbshot.save(json_string)
 
     def batchImport(self):
+        if not self.connected:
+            return 1
         files = os.listdir(self.folderPath)
         for filename in files:
             path = os.path.join(self.folderPath, filename)
             if os.path.isfile(path):
                 if filename.endswith('json'):
-                    self.importFile(path)
+                    fileClass = path.split('/')[-1]
+                    with open(path, "r+", encoding='utf-8') as jsonFile:
+                        data = json.load(jsonFile)
+                        if fileClass.startswith("Program"):
+                            id = data["Metadata"]["ParentID"]
+                            data["_id"] = id
+                        elif fileClass.startswith("Video"):
+                            id = data["Metadata"]["VideoID"]
+                            data["_id"] = id
+                        json_string = json.dumps(data, indent=4, ensure_ascii=False)
+                        self.importFile(json_string, fileClass)
             elif os.path.isdir(path):
                 self.batchImport(path)
+        return 0
 
 
 if __name__ == '__main__':
