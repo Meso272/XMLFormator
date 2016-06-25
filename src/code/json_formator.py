5af54d1d75969a180d3c5a251d669cb17163dbe9
周鲁东 <ludongzhou@gmail.com>
2016-06-25 16:02:32 +0800

    rebuild JsonValidation.jar. no need to specify main class now; update json_formator.py

--- src/code/json_formator.py
+++ src/code/json_formator.py
@@ -5,7 +5,7 @@ class JSONFormator:
 
     # json_path 是一个文件夹, 递归的遍历json_path 所代表的文件夹, 将发现的json 格式化一下
     def format(self, json_path):
-        subprocess.call(['java', '-cp', 'MongoValidation.jar', 'search.validation.JsonValidation', json_path])
+        subprocess.call(['java', '-jar', 'JsonValidation.jar', json_path])
 
 
 if __name__ == "__main__":
