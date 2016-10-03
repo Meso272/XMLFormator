23e0deff0ac0c1ac4773e8921f28f41c932e04e1
ludongzhou <ludongzhou@gmail.com>
2016-10-03 15:35:23 +0800

    add Personal xsl

--- src/code/SqlTrigger.py
+++ src/code/SqlTrigger.py
@@ -50,6 +50,10 @@ class SQLTrigger:
             video_price = row["video_price"]
             video_copyright = row["video_copyright"]
             xsl_folder = '../xsl/1'
+
+            if 'personal_xml' in xml_upload_path:
+                xsl_folder = '../xsl/personal'
+
             xml_trans_path = row["xml_trans_path"]
             video_play_path = row["video_play_path"]
     
